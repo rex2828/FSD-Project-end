da067db9c1226a02c47a8b4b5cef2171c36e582f
Sudeep Gangwar <75942034+rex2828@users.noreply.github.com>
2022-06-01 02:16:41 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-// mongodb+srv://sudeep:fsdproject@cluster0.hohd1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
 const MONGO_URI = 'mongodb://localhost:27017/fsd';
 
